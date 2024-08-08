dfa0dd6caa4bd0a57bcd3e395c34350b7b1b8add
Ujjwal Prakash <Ujjwalprakash144@gmail.com>
2024-08-08 13:30:36 +0530

    trying to fix client error

--- server/index.js
+++ server/index.js
@@ -1,44 +1,49 @@
-import express from 'express';
-import cors from 'cors';
-import { configDotenv } from 'dotenv';
-import userAuthRoutes from './Routes/userAuthRoutes.js';
-import userDetailsRoutes from './Routes/userDetailsRoutes.js';
-import userPostRoutes from './Routes/userPostRoutes.js';
-import connectDB from './Config/db.js';
+import express from "express";
+import cors from "cors";
+import { configDotenv } from "dotenv";
+import userAuthRoutes from "./Routes/userAuthRoutes.js";
+import userDetailsRoutes from "./Routes/userDetailsRoutes.js";
+import userPostRoutes from "./Routes/userPostRoutes.js";
+import connectDB from "./Config/db.js";
 
 const app = express();
 configDotenv();
 
-//MiddleWare 
-app.use(cors({
-    origin : '*',
-    methods : ['GET', 'POST', 'PUT', 'DELETE'],
-})); //used to enable cors policy
+//MiddleWare
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+); //used to enable cors policy
 // Increase payload size limit
-app.use(express.json({ limit: '2mb' }));//used to parse json request from client side 
-app.use(express.urlencoded({ limit: '2mb', extended: true }));//used to parse url encoded request from postman
-
+app.use(express.json({ limit: "2mb" })); //used to parse json request from client side
+app.use(express.urlencoded({ limit: "2mb", extended: true })); //used to parse url encoded request from postman
 
 //Connecting to MongoDB database
 connectDB();
 
-//Home route 
-app.get("/", (req, res)=> {
-    res.send("Ujjwal Welcomes you to the MERN Stack Blogging Application");
+//Home route
+// app.get("/", (req, res)=> {
+//     res.send("Ujjwal Welcomes you to the MERN Stack Blogging Application");
+// });
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 //Routes to handle authentication requests
-app.use('/api/auth/', userAuthRoutes);
+app.use("/api/auth/", userAuthRoutes);
 
 //Routes to CRUD the user Details
-app.use('/api/user/', userDetailsRoutes);
+app.use("/api/user/", userDetailsRoutes);
 
 //Routes to CRUD the posts of the user
-app.use('/api/post/', userPostRoutes);
-
+app.use("/api/post/", userPostRoutes);
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
